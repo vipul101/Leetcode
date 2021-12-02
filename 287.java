2fcda70831197b581727d70a7f0affe7887542e2
vipul101 <56825694+vipul101@users.noreply.github.com>
2021-12-02 17:42:33 +0530

    287. Find the Duplicate Number

---
+++ 287.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        for(int i=0;i<nums.length;i++){
+            if(nums[Math.abs(nums[i])] < 0){
+                return Math.abs(Math.abs(nums[i]));
+            }else{
+                nums[Math.abs(nums[i])] *= -1;
+            }
+        }
+        return -1;
+    }
+}
